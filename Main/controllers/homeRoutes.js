fefbf4fb794f790560e21a16d107efcd48ef7524
npcoiner <npcoiner@ucdavis.edu>
2024-03-06 19:21:30 -0800

    Added button to swap to v2 prototype

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -27,6 +27,31 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/v2', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepagev2', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/project/:id', async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
